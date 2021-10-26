783c67e0c1d8de98ca42cb7e43098124b30e8f17
ajbaenarosino <ajbaenarosino@correo.ugr.es>
2021-10-26 12:39:56 +0200

    Class methods declared

--- estudiante/include/maxqueue.h
+++ estudiante/include/maxqueue.h
@@ -3,3 +3,44 @@
  * @brief  Archivo de especificación del TDA MaxQueue
  * @author
  */
+
+#include <bits/stdc++.h>
+#include <stack>
+#include "element.h"
+
+#ifndef _MAXQUEUE_H_
+#define _MAXQUEUE_H_
+
+template <class T> class MaxQueue {
+
+    stack<T> Stack;
+
+    public:
+        MaxQueue<T>();
+
+        MaxQueue<T>(vector<T> data);
+
+        MaxQueue<T>(T* data, int n);
+
+        ~MaxQueue();
+
+        const T &front() const;
+
+        T &front();
+
+        const int &size() const;
+
+        void pop();
+
+        void push_back();
+
+        void assign(vector<T> data);
+
+        void assign(T* data, int n);
+
+        void destroy();
+
+        friend ostream &operator<<<T>(ostream &os, MaxQueue<T> out);
+};
+
+#endif
--- estudiante/src/maxqueue.cpp
+++ estudiante/src/maxqueue.cpp
@@ -3,3 +3,87 @@
  * @brief  Archivo de implementación del TDA MaxQueue
  * @author
  */
+
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <iostream>
+#include <stack>
+#include <vector>
+#include "element.h"
+#include "maxqueue.h"
+
+using namespace std;
+
+template <class T>
+MaxQueue<T>::MaxQueue()
+{
+}
+
+template <class T>
+MaxQueue<T>::MaxQueue(vector<T> data)
+{
+    assign(data);
+}
+
+template <class T>
+MaxQueue<T>::MaxQueue(T *data, int n)
+{
+    assign(data, n);
+}
+
+template <class T>
+MaxQueue<T>::~MaxQueue()
+{
+}
+
+template <class T>
+const T &MaxQueue<T>::front() const
+{
+    return Stack.top();
+}
+
+template <class T>
+T &MaxQueue<T>::front()
+{
+    return Stack.top();
+}
+
+template <class T>
+const int &MaxQueue<T>::size() const {
+    return Stack.size();
+}
+
+template <class T>
+void MaxQueue<T>::pop()
+{
+    Stack.pop();
+}
+
+template <class T>
+void MaxQueue<T>::push_back()
+{
+}
+
+template <class T>
+void MaxQueue<T>::assign(vector<T> data)
+{
+}
+
+template <class T>
+void MaxQueue<T>::assign(T *data, int n)
+{
+}
+
+template <class T>
+void MaxQueue<T>::destroy()
+{
+    for (int i = 0; i < Stack.size(); i++) {
+        Stack.pop();
+    }
+}
+
+template <class T>
+ostream &operator<<(ostream &os, MaxQueue<T> &x)
+{
+    return os;
+}
