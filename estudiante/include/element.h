f64e4772e6628228219f9b22868300b6d81ffedd
alejandrotm22 <torresmartinezalejandro2@gmail.com>
2021-11-06 20:04:55 +0100

    documentation and stack finished

--- estudiante/include/element.h
+++ estudiante/include/element.h
@@ -1,7 +1,7 @@
 /**
  * @file maxqueue.h
- * @brief  Archivo de especificación del TDA MaxQueue
- * @author
+ * @brief Specification file of the element TDA
+ * @authors Álvaro Jesús Baena Rosino \n Alejandro Torres Martínez
  */
 
 #include <cstdlib>
@@ -11,16 +11,43 @@ using namespace std;
 #ifndef _ELEMENT_H_
 #define _ELEMENT_H_
 
+/**
+ * @brief A template struct to represent a element made up by a T class value and an associated maximum
+*/
+
 template <class T>
 struct element {
+    /**
+     * @brief The value itself
+    */
     T value;
+
+    /**
+     * @brief The associated maximum
+    */
     T maximum;
-    template <class U> friend ostream &operator<<(ostream &os, element<U> &out);
+
+    /**
+     * @brief The default constructor
+     * @param v The value to be asigned
+     * @param max The associated maximum to be assigned
+    */
+    element(int v = 0, int max = 0);
+
+    /**
+     * @brief The output operator
+     * @param os The ostream object in which the object will be printed
+     * @param out The element to be printed
+    */
+    template <class U> friend ostream &operator<<(ostream &os, const element<U> &out);
 };
 
+template <class T>
+element<T>::element(int v, int max): value(v), maximum(max){}
+
 template <class U>
-ostream &operator<<(ostream &os, element<U> &out) {
-    os << out.value << "|" << out.maximum;
+ostream &operator<<(ostream &os, const element<U> &out) {
+    os << out.value << "," << out. maximum;
     return os;
 }
 
--- estudiante/src/cola_max.cpp
+++ estudiante/src/cola_max.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file cola_max.cpp
+ * @brief Main function to test the TDA of MaxQueue
+ * @authors Álvaro Jesús Baena Rosino \n Alejandro Torres Martínez
+ */
+
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
@@ -9,15 +15,35 @@ using namespace std;
 int main(int argc, char *argv[]){
 
     // // Run the current exercise
-    // MaxQueue queue;
+    MaxQueue<int> queue;
+    ifstream in;
+    if (argv[1][0] == '.' && argv[1][1] == '/' || argv[1][0] == '/') { // Additional functionality: You can test the queue with a plain text file, apart from the default args entry
+        in.open(argv[1]);
+        if(in.good()) {
+            while(!in.eof()) {
+                string aux;
+                in >> aux;
+                if (aux[0] == '.'){
+                    cout << queue.front() << endl;
+                    queue.pop();
+                } else {
+                    queue.push_back(stoi(aux));
+                }
+            }
+        } else {
+            cerr << "Invalid file" << endl;
+        }
+    } else {
+        for(int i = 1; i < argc; i++){
+            if (argv[i][0] == '.'){
+                cout << queue.front() << endl;
+                queue.pop();
+            } else {
+                queue.push_back(atoi(argv[i]));
+            }
+        }
+    }
 
-    // for(int i = 1; i < argc; i++){
-    //     if (argv[i][0] == '.'){
-    //         cout << queue.front() << endl;
-    //         queue.pop();
-    //     } else {
-    //         queue.push(atoi(argv[i]));
-    //     }
-    // }
-    // return 0;
+    in.close();
+    return 0;
 }
